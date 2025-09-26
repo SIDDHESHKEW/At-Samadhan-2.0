2d7d1a4c1e11ca988ecf6030b88ec9ba33e78e62
SIDDHESHKEW <siddheshkewate@gmail.com>
2025-09-27 05:13:37 +0530

    Delete NeuroBoost directory

--- NeuroBoost/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # <--- Add 'include' here
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('NeuroBoostApp.urls')),  # <--- Add this line
-]
